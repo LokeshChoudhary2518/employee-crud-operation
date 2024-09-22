f55521ee194bb75700e7246b27a23297fbf0ebd6
Lokesh <lokeshchoudhary2518@gmail.com>
2024-09-22 13:15:20 +0530

    empty folder structure

---
+++ employee-crud-operation/src/test/java/com/employee_crud_operation/EmployeeCrudOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee_crud_operation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCrudOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
